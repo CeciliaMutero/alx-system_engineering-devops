c8aaca1d92da2ac42c3b656a62a48f46c751a26f
Cecilia <cecilmutero66@gmail.com>
2024-05-22 20:02:52 -0700

    0. Nginx likes port 80

---
+++ 0x05-processes_and_signals/open_and_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script for Terminal 0
+gnome-terminal --title="Terminal 0" -- ./4-to_infinity_and_beyond
+
+# Script for Terminal 1
+gnome-terminal --title="Terminal 1" -- ./5-dont_stop_me_now
